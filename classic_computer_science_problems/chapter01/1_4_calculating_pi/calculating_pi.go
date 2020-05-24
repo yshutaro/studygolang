57ab6823bd37bdf7ba82bd8691f5b718869cb8c5
yshutaro <yshutaro@gmail.com>
2020-05-24 23:37:50 +0900

    変数名と関数名をリファクタリング

--- classic_computer_science_problems/chapter01/1_4_calculating_pi/calculating_pi.go
+++ classic_computer_science_problems/chapter01/1_4_calculating_pi/calculating_pi.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(calculate_pi(1000000))
+	fmt.Println(calculatePi(1000000))
 }
 
-func calculate_pi(n_terms int) float64 {
+func calculatePi(nTerms int) float64 {
 	numerator := 4.0
 	denominator := 1.0
 	operation := 1.0
 	pi := 0.0
-	for i := 0; i < n_terms; i++ {
+	for i := 0; i < nTerms; i++ {
 		pi += operation * (numerator / denominator)
 		denominator += 2.0
 		operation *= -1.0
